5f9ebd7b504f4248fa3903d17ce86c9633474f99
Shogo Ikeda <shogoa24@gmail.com>
2025-08-07 02:48:17 +0900

    Add solution for Length of Last Word

--- LeetCode/Length-of-Last-Word/Solution.py
+++ LeetCode/Length-of-Last-Word/Solution.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         l=0
         i = len(s)-1
-        while i >= 0 and s[i]==' ':
+        while i > 0 and s[i]==' ':
             i -= 1
         while i>=0 and s[i]!=' ':
             l += 1
