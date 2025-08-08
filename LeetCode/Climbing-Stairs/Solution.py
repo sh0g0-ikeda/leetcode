1547b6a8381b6b04298ba24e12fa1d31ea0b2f10
Shogo Ikeda <shogoa24@gmail.com>
2025-08-09 02:13:44 +0900

    Add solution for Climbing Stairs

---
+++ LeetCode/Climbing-Stairs/Solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        a, b = 1, 1          
+        for _ in range(n-1): 
+            a, b = b, a + b  
+        return b             
+
