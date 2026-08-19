99dc122045e28361aaa7becdcfc55f33533aac25
Shogo Ikeda <shogoa24@gmail.com>
2026-08-19 20:42:53 +0900

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.py
+++ LeetCode/Group-Anagrams/Solution.py
@@ -1,16 +1,13 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        dic={}
-        for i in strs:
-            x=sorted(i)
-            key=''.join(x)
-            if key not in dic:
-                dic[key]=[]
-            dic[key].append(i)
-        ans=list(dic.values())
-        return ans
-
+1class Solution:
+2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+3        
+4        groups={}
+5
+6        for i in strs:
+7            key="".join(sorted(i))
+8            if key not in groups:
+9                groups[key]=[]
+10            groups[key].append(i)
+11
+12        return list(groups.values())
+13
