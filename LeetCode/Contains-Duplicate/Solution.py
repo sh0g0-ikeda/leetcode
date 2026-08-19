9ff5740f5a5a4abfbe190b5b2ba7896b2530e01a
Shogo Ikeda <shogoa24@gmail.com>
2026-08-19 18:32:24 +0900

    Add solution for Contains Duplicate

--- LeetCode/Contains-Duplicate/Solution.py
+++ LeetCode/Contains-Duplicate/Solution.py
@@ -1,1 +1,11 @@
-return False if len(nums) == len(set(nums)) else True
+1class Solution:
+2    def containsDuplicate(self, nums: List[int]) -> bool:
+3        seen=set()
+4        for i in nums:
+5            if i in seen:
+6                return True
+7            seen.add(i)
+8
+9        return False
+10
+11
