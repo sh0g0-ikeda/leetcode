9026d81fc09483b9392163a43bac1d140546b9ac
Shogo Ikeda <shogoa24@gmail.com>
2026-08-19 22:58:24 +0900

    Add solution for Valid Palindrome

--- LeetCode/Valid-Palindrome/Solution.py
+++ LeetCode/Valid-Palindrome/Solution.py
@@ -1,20 +1,18 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-
-        test=reversed(s)
-        if test==s:
-            return True
-
-        snew= "".join(ch for ch in s if ch.isalnum())
-        snew=snew.lower()
-        srev=''.join(reversed(snew))
-
-        if snew==srev:
-            return True
-        else:
-            return False
-
+1class Solution:
+2    def isPalindrome(self, s: str) -> bool:
+3        clean = ""
+4
+5        for c in s:
+6            if c.isalnum():
+7                clean += c.lower()
+8        n=len(clean)
+9        m=len(clean)//2
+10        for i in range(m):
+11            if clean[i] != clean[n-1-i]:
+12                return False
+13
+14        return True
+15
+16
+17            
+18
