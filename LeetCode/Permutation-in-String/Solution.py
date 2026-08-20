ade089aa9662714faf7a77a14b1c7b0ebe31b5c2
Shogo Ikeda <shogoa24@gmail.com>
2026-08-20 20:36:52 +0900

    Add solution for Permutation in String

--- LeetCode/Permutation-in-String/Solution.py
+++ LeetCode/Permutation-in-String/Solution.py
@@ -1,11 +1,12 @@
 1class Solution:
 2    def checkInclusion(self, s1: str, s2: str) -> bool:
 3        n=len(s1)
-4
-5        for i in range(len(s2)-n+1):
-6            if sorted(s1)==sorted(s2[i:i+n]):
-7                return True
-8        return False
+4        for l in range(len(s2)-n+1):
+5            if sorted(s1)==sorted(s2[l: l+n]):
+6                return True
+7        return False
+8
 9
 10
 11
+12
